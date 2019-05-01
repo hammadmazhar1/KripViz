aa9795b911302a954858773456f75f973442d0ae
jeehan24 <jeehanmalik24@gmail.com>
2019-05-01 17:20:13 -0500

    Add files via upload

--- graphStructure.py
+++ graphStructure.py
@@ -1,10 +1,10 @@
-from tkinter import Tk, Canvas, Frame, Button, Label, Entry, END, LEFT, RIGHT, SUNKEN
-from graphviz import Digraph
+from tkinter import *
+#from graphviz import Digraph
 class KripkeFrame:
 
-    def __init__(self, states, formulas, programs):
+    def __init__(self, states):
         #list of states(strings) in the frame
-        self.states = []
+        self.states = states
         
         #dictionary, where keys are the formulas and values are lists containing states where formulas are true
         #e.g
@@ -39,7 +39,7 @@ class KripkeFrame:
     def addProgram(self, programName, state1 = None, state2 = None):
         if state1 is not None and state2 is not None:
             if programName not in self.programs:
-                self.programs[programName] = (state1, state2)
+                self.programs[programName] = [(state1, state2)]
             else:
                 if (state1, state2) in self.programs[programName]:
                     raise ValueError("This edge is already in the frame")
@@ -52,8 +52,12 @@ class KripkeFrame:
                 raise ValueError("This program is already in the frame")
         else:
             raise ValueError("Enter start state and end state (Both or None) ")
+        
+    def __repr__(self):
+        return "<states: {}> , formulas: {} , programs: {} ".format(self.states, self.formulas, self.programs)
 
 
+'''
 def genFrame(kripkeFrame):
     kripkeFrame.graph = Digraph('curGraph',format='gif')
     for state in kripkeFrame.states:
@@ -66,29 +70,82 @@ def genFrame(kripkeFrame):
     for program in kripkeFrame.programs.keys():
         kripkeFrame.graph.edges(kripkeFrame.programs[program])
     kripkeFrame.graph.render()
-    
+'''
 
+#CALLBACK FUNCTIONS
 
 def initializeNewStructure():
-    pass
+    #read all entry boxes
+    
+    states = statesEntry.get().split()
 
+    formula1Name = formulaEntry.get().strip()
+    formula1States = formulaStateEntry.get().split()
+
+    formula2Name = formula2Entry.get().strip()
+    formula2States = formula2StateEntry.get().split()
+
+    formula3Name = formula3Entry.get().strip()
+    formula3States = formula3StateEntry.get().split()
+
+    program1Name = programNameEntry.get().strip()
+    program1State1 = programState1Entry.get().strip()
+    program1State2 = programState2Entry.get().strip()
+
+    program2Name = program2NameEntry.get().strip()
+    program2State1 = program2State1Entry.get().strip()
+    program2State2 = program2State2Entry.get().strip()
+
+    program3Name = program3NameEntry.get().strip()
+    program3State1 = program3State1Entry.get().strip()
+    program3State2 = program3State2Entry.get().strip()
+
+    program4Name = program4NameEntry.get().strip()
+    program4State1 = program4State1Entry.get().strip()
+    program4State2 = program4State2Entry.get().strip()
+
+    formed_frame = KripkeFrame(states)
+    formed_frame.addFormula(formula1Name, formula1States)
+    formed_frame.addFormula(formula2Name, formula2States)
+    formed_frame.addFormula(formula3Name, formula3States)
+    formed_frame.addProgram(program1Name, program1State1, program1State1)
+    formed_frame.addProgram(program2Name, program2State1, program2State1)
+    formed_frame.addProgram(program3Name, program3State1, program3State1)
+    formed_frame.addProgram(program4Name, program4State1, program4State1)
+    print(formed_frame)
+
+    #make dot file for this frame
+
+
+    #display formed file on canvas
+    gif1 = PhotoImage(file='test1.gif')
+    canvas.create_image(0,0,image=gif1, anchor=NW)
 
 def createGUI():
     global rootWindow
     global canvas
-    global statusLabel
-    global whichHeapEntry
-    global numBallsEntry
-    global newHeapEntry
-    global playerWin
-    global computerWin
-    global wonBoardLabel
+    global statesEntry
+    global formulaEntry
+    global formulaStateEntry
+    global formula2Entry
+    global formula2StateEntry
+    global formula3Entry
+    global formula3StateEntry
+    global programNameEntry
+    global programState1Entry
+    global programState2Entry
+    global program2NameEntry
+    global program2State1Entry
+    global program2State2Entry
+    global program3NameEntry
+    global program3State1Entry
+    global program3State2Entry
+    global program4NameEntry
+    global program4State1Entry
+    global program4State2Entry
 
 
-    canvasHeight = 620
-    canvasWidth = 750
-    canvasBorderBuffer = 10
-    maxBallSize = 50
+    #maxBallSize = 50
 
 
     playerWin = 0
@@ -99,37 +156,121 @@ def createGUI():
     
     canvasAndGUI = Frame(rootWindow)
     canvasAndGUI.configure(background='green')
+
+
+    
+    canvasHeight = 620
+    canvasWidth = 620
+    canvasBorderBuffer = 10
     canvas = Canvas(canvasAndGUI, height=canvasHeight, width=canvasWidth, relief=SUNKEN, borderwidth=2)
     canvas.pack(side=LEFT)
+    gif1 = PhotoImage(file='test2.gif')
+    canvas.create_image(0,0,image=gif1, anchor=NW)
 
 
     guiFrame = Frame(canvasAndGUI)
-    wonBoardLabel = Label(guiFrame, text='Enter Details')
+    wonBoardLabel = Label(guiFrame, text='Enter Details',width= 50, height = 4)
     wonBoardLabel.pack()
 
+   
     kripkeStatesDetails = Frame(guiFrame)
-    stateLabel = Label(kripkeStatesDetails, text='States:')
+    stateLabel = Label(kripkeStatesDetails, text='States:',width= 8, height = 3)
     statesEntry = Entry(kripkeStatesDetails)
     stateLabel.pack(side=LEFT)
     statesEntry.pack(side=LEFT)
        
-    KripkeFormulasFrame = Frame(guiFrame)
-    formulaLabel = Label(KripkeFormulasFrame, text='Formulas:')
-    formulaEntry = Entry(KripkeFormulasFrame)
+    KripkeFormula1Frame = Frame(guiFrame)
+    formulaLabel = Label(KripkeFormula1Frame, text='Formula:',width= 9)
+    formulaEntry = Entry(KripkeFormula1Frame,width= 4)
+    formulaStateLabel = Label(KripkeFormula1Frame, text='True in states:',width= 12)
+    formulaStateEntry = Entry(KripkeFormula1Frame,width= 10)
     formulaLabel.pack(side=LEFT)
     formulaEntry.pack(side = LEFT)
+    formulaStateLabel.pack(side = LEFT)
+    formulaStateEntry.pack(side = LEFT)
 
-    KripkeProgramsFrame = Frame(guiFrame)
-    programLabel = Label(KripkeProgramsFrame, text='Programs:')
-    programEntry = Entry(KripkeProgramsFrame)
-    programLabel.pack(side=LEFT)
-    programEntry.pack(side = LEFT)
+    KripkeFormula2Frame = Frame(guiFrame)
+    formula2Label = Label(KripkeFormula2Frame, text='Formula:',width= 9)
+    formula2Entry = Entry(KripkeFormula2Frame,width= 4)
+    formula2StateLabel = Label(KripkeFormula2Frame, text='True in states:',width= 12)
+    formula2StateEntry = Entry(KripkeFormula2Frame,width= 10)
+    formula2Label.pack(side=LEFT)
+    formula2Entry.pack(side = LEFT)
+    formula2StateLabel.pack(side = LEFT)
+    formula2StateEntry.pack(side = LEFT)
+
+    
+    KripkeFormula3Frame = Frame(guiFrame)
+    formula3Label = Label(KripkeFormula3Frame, text='Formula:',width= 9)
+    formula3Entry = Entry(KripkeFormula3Frame,width= 4)
+    formula3StateLabel = Label(KripkeFormula3Frame, text='True in states:',width= 12)
+    formula3StateEntry = Entry(KripkeFormula3Frame,width= 10)
+    formula3Label.pack(side=LEFT)
+    formula3Entry.pack(side = LEFT)
+    formula3StateLabel.pack(side = LEFT)
+    formula3StateEntry.pack(side = LEFT)
     
 
+    KripkeProgram1Frame = Frame(guiFrame)
+    programNameLabel = Label(KripkeProgram1Frame, text='Program:', width= 8, height = 4)
+    programNameEntry = Entry(KripkeProgram1Frame,width= 4)
+    programState1Label = Label(KripkeProgram1Frame, text='goes from state: ', width= 14)
+    programState1Entry =  Entry(KripkeProgram1Frame,width= 4)
+    programState2Label = Label(KripkeProgram1Frame, text='to state: ', width= 8)
+    programState2Entry =  Entry(KripkeProgram1Frame,width= 4)
+    programNameLabel.pack(side=LEFT)
+    programNameEntry.pack(side = LEFT)
+    programState1Label.pack(side = LEFT)
+    programState1Entry.pack(side = LEFT)
+    programState2Label.pack(side = LEFT)
+    programState2Entry.pack(side = LEFT)
+
+    KripkeProgram2Frame = Frame(guiFrame)
+    program2NameLabel = Label(KripkeProgram2Frame, text='Program:', width= 8)
+    program2NameEntry = Entry(KripkeProgram2Frame,width= 4)
+    program2State1Label = Label(KripkeProgram2Frame, text='goes from state: ', width= 14)
+    program2State1Entry =  Entry(KripkeProgram2Frame,width= 4)
+    program2State2Label = Label(KripkeProgram2Frame, text='to state: ', width= 8)
+    program2State2Entry =  Entry(KripkeProgram2Frame,width= 4)
+    program2NameLabel.pack(side=LEFT)
+    program2NameEntry.pack(side = LEFT)
+    program2State1Label.pack(side = LEFT)
+    program2State1Entry.pack(side = LEFT)
+    program2State2Label.pack(side = LEFT)
+    program2State2Entry.pack(side = LEFT)
+
+    KripkeProgram3Frame = Frame(guiFrame)
+    program3NameLabel = Label(KripkeProgram3Frame, text='Program:', width= 8)
+    program3NameEntry = Entry(KripkeProgram3Frame,width= 4)
+    program3State1Label = Label(KripkeProgram3Frame, text='goes from state: ', width= 14)
+    program3State1Entry =  Entry(KripkeProgram3Frame,width= 4)
+    program3State2Label = Label(KripkeProgram3Frame, text='to state: ', width= 8)
+    program3State2Entry =  Entry(KripkeProgram3Frame,width= 4)
+    program3NameLabel.pack(side=LEFT)
+    program3NameEntry.pack(side = LEFT)
+    program3State1Label.pack(side = LEFT)
+    program3State1Entry.pack(side = LEFT)
+    program3State2Label.pack(side = LEFT)
+    program3State2Entry.pack(side = LEFT)
+
+
+    KripkeProgram4Frame = Frame(guiFrame)
+    program4NameLabel = Label(KripkeProgram4Frame, text='Program:', width= 8, height = 4)
+    program4NameEntry = Entry(KripkeProgram4Frame,width= 4)
+    program4State1Label = Label(KripkeProgram4Frame, text='goes from state: ', width= 14)
+    program4State1Entry =  Entry(KripkeProgram4Frame,width= 4)
+    program4State2Label = Label(KripkeProgram4Frame, text='to state: ', width= 8)
+    program4State2Entry =  Entry(KripkeProgram4Frame,width= 4)
+    program4NameLabel.pack(side=LEFT)
+    program4NameEntry.pack(side = LEFT)
+    program4State1Label.pack(side = LEFT)
+    program4State1Entry.pack(side = LEFT)
+    program4State2Label.pack(side = LEFT)
+    program4State2Entry.pack(side = LEFT)
     #execButton = Button(guiFrame, text='Do it!', command=executePlayerMove)
 
 
-    newGameButton = Button(guiFrame, text='Create Frame', command=initializeNewStructure)
+    createFrameButton = Button(guiFrame, text='Create Frame', command=initializeNewStructure)
 
     # replace the widget below with an Entry widget
     #newHeapLabel = Label(guiFrame, text='New Frame')
@@ -139,15 +280,32 @@ def createGUI():
     
 
     kripkeStatesDetails.pack()
-    KripkeFormulasFrame.pack()
-    KripkeProgramsFrame.pack()
     
-    #execButton.pack()
-    #newHeapLabel.pack(side=LEFT)
-    #newHeapEntry.pack()
-    newGameButton.pack()
+    KripkeFormula1Frame.pack()
+    KripkeFormula2Frame.pack()
+    KripkeFormula3Frame.pack()
+    #KripkeFormula4Frame.pack()
+    
+    KripkeProgram1Frame.pack()
+    KripkeProgram2Frame.pack()
+    KripkeProgram3Frame.pack()
+    KripkeProgram4Frame.pack()
+    
+    
+    createFrameButton.pack()
+    
+   
     guiFrame.pack(side=RIGHT)
     canvasAndGUI.pack()
-    statusLabel = Label(rootWindow)
-    statusLabel.pack()
-createGUI()
+
+
+    mainloop()
+
+    
+
+def runGUI():
+    createGUI()
+    rootWindow.mainloop()
+
+
+runGUI()
